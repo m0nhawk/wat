8f0056fcdd4b7845e2b053bf1ae4e10711cbb9c1
Andrew Prokhorenkov <andrew.prokhorenkov@gmail.com>
2017-04-15 14:09:05 +0300

    run.bat extension with creating ui_main.py

--- run.bat
+++ run.bat
@@ -1,8 +1,16 @@
 @echo off
 
 setlocal
+
 set PYTHONPATH=%PYTHONPATH%;%cd%;%cd%\src
 
-python wat.py
+if not exist "ui_main.py" (
+    pyuic5 main.ui -o ui_main.py
+
+    set PYTHONPATH=%PYTHONPATH%;%cd%;%cd%\src
+    python wat.py
+) else (
+    python wat.py
+)
 
 endlocal
